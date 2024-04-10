23cc557eeee1d728d7d67fd0e1af5fca95e3e827
CuogFan <mineluciferlol@gmail.com>
2024-04-10 22:29:47 +0700

    filter added

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "HomeController", urlPatterns = "")
+@WebServlet(name = "HomeController", urlPatterns = "/")
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
